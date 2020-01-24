ead0a0d08d63600d6febb4b0deac149009559c52
stesim <simeonov.stefan@gmail.com>
2020-01-24 23:36:33 +0100

    disabled auto-activation of updated service worker

--- sw.js
+++ sw.js
@@ -17,8 +17,6 @@ const staticCacheName = `static-cache-v${staticCacheVersion}`;
 const dynamicCacheName = `dynamic-cache-v${dynamicCacheVersion}`;
 
 self.addEventListener('install', (event) => {
-  // TODO(stesim): remove skipWaiting()
-  self.skipWaiting();
   event.waitUntil(
     caches.open(staticCacheName).then((cache) => {
       cache.addAll(staticCacheResources);
