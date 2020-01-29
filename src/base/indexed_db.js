59d2abdb5d1809f52addc67896517091de1abcef
stesim <simeonov.stefan@gmail.com>
2020-01-29 01:21:55 +0100

    promise-based IndexedDB wrapper class

---
+++ src/base/indexed_db.js
@@ -0,0 +1,89 @@
+class TransactionStore {
+  constructor(store) {
+    this._store = store;
+  }
+
+  add(value, key) {
+    return new Promise((resolve, reject) => {
+      const request = this._store.add(value, key);
+      request.onsuccess = (evt => resolve(evt.target.result));
+      request.onerror = (evt => reject(evt.target.error));
+    });
+  }
+}
+
+export default class IndexedDB {
+  constructor(name, version, onOpen, onError, onUpgradeNeeded) {
+    const request = indexedDB.open(name, version);
+    request.onsuccess = (evt) => {
+      this._db = evt.target.result;
+      if (onOpen) {
+        onOpen();
+      }
+    };
+    request.onerror = (evt) => {
+      if (onError) {
+        onError(evt.target.error);
+      }
+    };
+    request.onupgradeneeded = (evt) => {
+      if (onUpgradeNeeded) {
+        this._upgradeDb = evt.target.result;
+        onUpgradeNeeded(evt.oldVersion);
+        this._upgradeDb = null;
+      }
+    };
+    
+    this._db = null;
+    this._upgradeDb = null;
+  }
+
+  static open(name, version, onUpgradeNeeded) {
+    return new Promise((resolve, reject) => {
+      const db = new IndexedDB(
+        name,
+        version,
+        () => resolve(db),
+        (error) => reject(error),
+        (oldVersion) => onUpgradeNeeded(db, oldVersion));
+    });
+  }
+
+  containsStore(name) {
+    if (this._upgradeDb === null) {
+      throw new Error('IndexedDB.containsStore() can be called only from the upgradeNeeded callback');
+    }
+    return this._upgradeDb.objectStoreNames.contains(name);
+  }
+
+  createStore(name, options) {
+    if (this._upgradeDb === null) {
+      throw new Error('IndexedDB.createStore() can be called only from the upgradeNeeded callback');
+    }
+    return this._upgradeDb.createObjectStore(name, options);
+  }
+
+  transaction(stores, access, action) {
+    return new Promise((resolve, reject) => {
+      const transaction = this._db.transaction(stores, access);
+      transaction.oncomplete = () => {
+        resolve();
+      }
+      transaction.onabort = (evt) => reject(evt.target.error);
+      // transaction.onerror = (evt) => console.error('IndexedDB transaction error', evt.target.error);
+
+      if (Array.isArray(stores)) {
+        const transactionStores = {};
+        for (const store of stores) {
+          transactionStores[store] =
+            new TransactionStore(transaction.objectStore(store));
+        }
+        action(transactionStores);
+      } else {
+        const transactionStore =
+          new TransactionStore(transaction.objectStore(stores));
+        action(transactionStore);
+      }
+    });
+  }
+}
