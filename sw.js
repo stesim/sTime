f599e32bea4683d18d1df292a650e37bca64c045
stesim <simeonov.stefan@gmail.com>
2020-02-12 23:48:59 +0100

    bumped static cache version to 7 and dynamic cache version to 20

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 // NOTE: bump the static cache version if any of the statically cached resources
 // listed below have been modified, or if entries need to be added or removed
-const staticCacheVersion = 6;
+const staticCacheVersion = 7;
 
 // NOTE: bump the dynamic cache version if any of the resources loaded by the
 // static resources have been modified (e.g. JS or CSS files)
-const dynamicCacheVersion = 19;
+const dynamicCacheVersion = 20;
 
 const staticCacheResources = [
   'src/app.js',
